642c9baf5cf244074ecb33ddb35c08ae97903b7f
Stephen Cavilia <sac@atomicradi.us>
2019-08-14 12:56:34 -0400

    basic sensor data summary page

--- SacDaq-Web/lib/SacDaq/Web/Controller/Sensor.pm
+++ SacDaq-Web/lib/SacDaq/Web/Controller/Sensor.pm
@@ -2,6 +2,10 @@ package SacDaq::Web::Controller::Sensor;
 use Moose;
 use namespace::autoclean;
 
+use DateTime;
+use DateTime::Duration;
+use Statistics::Descriptive;
+
 BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
@@ -63,6 +67,66 @@ sub data :Chained('sensor') :Args(0) {
 
     my @reports = $s->search_related('reports', {}, {order_by => {-desc => 'time'}, rows => 50});
     $c->stash->{reports} = \@reports;
+}
+
+sub summary :Chained('sensor') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $s = $c->stash->{sensordef};
+    my $period = $c->req->param('period') // 'day';
+
+    my @intervals;
+    my ($trunc, $count, $interval, $span);
+
+    if ($period eq 'day')
+    {
+	$trunc = 'hour';
+	$count = 24;
+	$interval = DateTime::Duration->new(hours => 1);
+	$span = DateTime::Duration->new(minutes => 30);
+    }
+    if ($period eq 'week')
+    {
+	$trunc = 'day';
+	$count = 7;
+	$interval = DateTime::Duration->new(days => 1);
+	$span = DateTime::Duration->new(hours => 12);
+    }
+
+    my ($lastrep) = $s->search_related('reports', {},
+				       {order_by => {-desc=>'time'},
+					rows => 1});
+    my $last_time = $lastrep->time;
+    $c->log->debug("Most recent: $last_time");
+    my $trunc_time = $last_time->truncate(to => $trunc);
+    $c->log->debug("Most recent period: $trunc_time");
+    for (1..$count)
+    {
+	my $range_start = $trunc_time - $span;
+	my $range_end = $trunc_time + $span;
+	$c->log->debug("Next range: $range_start - $range_end ($trunc_time)");
+	push @intervals, [$trunc_time->clone, $range_start, $range_end];
+	$trunc_time -= $interval;
+    }
+
+    for my $interval(@intervals)
+    {
+	$c->log->debug("Looking up $interval->[0]");
+	my @reports = $s->search_related('reports', {time => {-between => [$interval->[1], $interval->[2]]}},
+					 {order_by => {-desc => 'time'}});
+	
+	my $stat = Statistics::Descriptive::Full->new();
+	for my $rep (@reports)
+	{
+	    my $t = $rep->time;
+	    $stat->add_data($rep->value);
+	}
+	push @$interval, $stat->mean, $stat->min, $stat->max;
+	$c->log->debug("Average: $interval->[3]");
+	$c->log->debug("Min: $interval->[4]");
+	$c->log->debug("Max: $interval->[5]");
+    }
+    $c->stash->{intervals} = \@intervals;
 }
 
 sub error :Private {
